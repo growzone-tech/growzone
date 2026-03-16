4370f8fa68cc0b4227df973f1ce7d66a4d9a8a20
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-03-17 00:59:33 +0100

    moves cfg into subdir, reads secrets in custom entrypoint

---
+++ traefik/entrypoint-hook.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+[ -f /run/secrets/PORKBUN_API_KEY ] && export PORKBUN_API_KEY="$(cat /run/secrets/PORKBUN_API_KEY 2>/dev/null)"
+[ -f /run/secrets/PORKBUN_API_SECRET_KEY ] && export PORKBUN_API_SECRET_KEY="$(cat /run/secrets/PORKBUN_API_SECRET_KEY 2>/dev/null)"
+
+traefik --configfile=/etc/traefik/traefik.yml
