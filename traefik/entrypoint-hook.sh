d85a5facbfa6d092b5eaa667c5c69e05f7ac18eb
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-03-17 01:08:06 +0100

    changes to replacing entrypoint and command

--- traefik/entrypoint-hook.sh
+++ traefik/entrypoint-hook.sh
@@ -3,4 +3,4 @@
 [ -f /run/secrets/PORKBUN_API_KEY ] && export PORKBUN_API_KEY="$(cat /run/secrets/PORKBUN_API_KEY 2>/dev/null)"
 [ -f /run/secrets/PORKBUN_API_SECRET_KEY ] && export PORKBUN_SECRET_API_KEY="$(cat /run/secrets/PORKBUN_API_SECRET_KEY 2>/dev/null)"
 
-traefik --configfile=/etc/traefik/traefik.yml
+exec "$@"
