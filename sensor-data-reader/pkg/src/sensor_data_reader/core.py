0c40730519641636561c3d7bd171b0c875709590
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-03-17 03:35:32 +0100

    does nothing for now

--- sensor-data-reader/pkg/src/sensor_data_reader/core.py
+++ sensor-data-reader/pkg/src/sensor_data_reader/core.py
@@ -1,6 +1,6 @@
 import threading
-import board
-import adafruit_dht
+# import board
+# import adafruit_dht
 import paho.mqtt.client as mqtt
 import os
 
@@ -10,7 +10,7 @@ MQTT_PORT = os.environ.get("MQTT_PORT")
 class Main:
     def __init__(self):
         self.__terminate = threading.Event()
-        self.__dht22 = adafruit_dht.DHT22(board.D26)
+        # self.__dht22 = adafruit_dht.DHT22(board.D26)
         self.__mqttClient = mqtt.Client()
 
     def connect_mqtt(self):
@@ -23,20 +23,20 @@ class Main:
         self.connect_mqtt()
         while not self.__terminate.is_set():
             try:
-                temperature = self.__dht22.temperature
-                humidity = self.__dht22.humidity
-                if temperature is not None:
-                    self.__mqttClient.publish("sensor/dht22/inside/temperature", f"{temperature:.2f}", retain=True)
-                if humidity is not None:
-                    self.__mqttClient.publish("sensor/dht22/inside/humidity", f"{humidity:.2f}", retain=True)
+                # temperature = self.__dht22.temperature
+                # humidity = self.__dht22.humidity
+                # if temperature is not None:
+                #     self.__mqttClient.publish("sensor/dht22/inside/temperature", f"{temperature:.2f}", retain=True)
+                # if humidity is not None:
+                #     self.__mqttClient.publish("sensor/dht22/inside/humidity", f"{humidity:.2f}", retain=True)
             except RuntimeError as error:
                 # This library throws a RuntimeError for almost every missed pulse.
                 # We just print and keep going.
-                print(f"Failed reading DHT22 at PIN 26: {error.args[0]}")
-                time.sleep(2.0)
+                # print(f"Failed reading DHT22 at PIN 26: {error.args[0]}")
+                self.__terminate.wait(2.0)
                 continue
             except Exception as error:
-                dht_device.exit()
+                # self.__dht22.exit()
                 raise error
             self.__terminate.wait(10.0)
 
