ab9eaf532de2a36e6a93d732efd11b6a50ddb3b5
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-03-30 21:53:17 +0200

    first attempt at adding authelia for authentication

---
+++ authelia/entrypoint-hook.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -d "/secrets" ]; then
+    mkdir -p /secrets
+fi
+if [ ! -f "/secrets/jwt" ]; then
+    head -c 64 /dev/urandom | base64 > /secrets/jwt
+fi
+if [ ! -f "/secrets/session" ]; then
+    head -c 64 /dev/urandom | base64 > /secrets/session
+fi
+if [ ! -f "/secrets/storage_key" ]; then
+    head -c 64 /dev/urandom | base64 > /secrets/storage_key
+fi
+
+exec "$@"
