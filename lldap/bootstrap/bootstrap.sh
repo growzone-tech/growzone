6873512176ff1edcf51c7190bd21ad6ce54aa2f6
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-03-31 18:13:07 +0200

    test bootstrapping again

--- lldap/bootstrap/bootstrap.sh
+++ lldap/bootstrap/bootstrap.sh
@@ -17,11 +17,21 @@ export USER_SCHEMAS_DIR="$tmpBootstrapDir/user-schemas"
 export GROUP_SCHEMAS_DIR="$tmpBootstrapDir/group-schemas"
 export DO_CLEANUP="false"
 
+[ -d "/bootstrap/user-configs" ] || mkdir -p "/bootstrap/user-configs"
+[ -d "/bootstrap/group-configs" ] || mkdir -p "/bootstrap/group-configs"
+[ -d "/bootstrap/user-schemas" ] || mkdir -p "/bootstrap/user-schemas"
+[ -d "/bootstrap/group-schemas" ] || mkdir -p "/bootstrap/group-schemas"
+[ -d "$tmpBootstrapDir/user-configs" ] || mkdir -p "$tmpBootstrapDir/user-configs"
+[ -d "$tmpBootstrapDir/group-configs" ] || mkdir -p "$tmpBootstrapDir/group-configs"
+[ -d "$tmpBootstrapDir/user-schemas" ] || mkdir -p "$tmpBootstrapDir/user-schemas"
+[ -d "$tmpBootstrapDir/group-schemas" ] || mkdir -p "$tmpBootstrapDir/group-schemas"
+
 (
-    cd "/bootstrap"
-    find user-configs group-configs user-schemas group-schemas -type d -exec mkdir -p "$tmpBootstrapDir/{}" \;
+    cd /bootstrap
     find user-configs group-configs user-schemas group-schemas -type f -exec sh -c 'envsubst < "$1" > "$2/$1"' _ {} "$tmpBootstrapDir" \;
 )
+
+cat "$tmpBootstrapDir/user-configs/admin.json"
 
 /app/bootstrap.sh
 touch "/data/.bootstrapped"
