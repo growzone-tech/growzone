573106e4f8b179142f46f78d3a6bfd9152b130db
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-04-06 18:05:42 +0200

    removes 81 from the ports routed through tailscale

--- tailscale/entrypoint-hook.sh
+++ tailscale/entrypoint-hook.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-socat TCP-LISTEN:80,bind=127.0.0.1,fork TCP:traefik:80 &
-#socat TCP-LISTEN:81,bind=127.0.0.1,fork TCP:traefik:81 &
-socat TCP-LISTEN:443,bind=127.0.0.1,fork TCP:traefik:443 &
-#socat TCP-LISTEN:53,bind=127.0.0.1,fork TCP:unbound:53 &
-#socat UDP-LISTEN:53,bind=127.0.0.1,fork UDP:unbound:53 &
+socat TCP-LISTEN:80,bind=127.0.0.1,fork "TCP:${APP_NAME_HOST}_traefik:80" &
+socat TCP-LISTEN:443,bind=127.0.0.1,fork "TCP:${APP_NAME_HOST}_traefik:443" &
 
 [ -n "$TS_AUTHKEY_FILE" ] && export TS_AUTHKEY="$(cat "$TS_AUTHKEY_FILE")"
 
