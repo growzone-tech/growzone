30298d2e7ef83e10bebc29c2e61da3d147af45a9
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-04-07 04:02:23 +0200

    passes udp 8189 through to media-mtx

--- tailscale/entrypoint-hook.sh
+++ tailscale/entrypoint-hook.sh
@@ -2,6 +2,7 @@
 
 socat TCP-LISTEN:80,bind=127.0.0.1,fork "TCP:${APP_NAME_HOST}_traefik:80" &
 socat TCP-LISTEN:443,bind=127.0.0.1,fork "TCP:${APP_NAME_HOST}_traefik:443" &
+socat UDP-LISTEN:8189,bind=127.0.0.1,fork "UDP:${APP_NAME_HOST}_traefik:8189" &
 
 [ -n "$TS_AUTHKEY_FILE" ] && export TS_AUTHKEY="$(cat "$TS_AUTHKEY_FILE")"
 
