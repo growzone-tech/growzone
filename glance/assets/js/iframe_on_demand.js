508def25b6c145cf640d42c3562ab111881140ae
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-04-09 19:02:48 +0200

    executes observer in root included script as soon as DOM is loaded

--- glance/assets/js/iframe_on_demand.js
+++ glance/assets/js/iframe_on_demand.js
@@ -1,26 +1,32 @@
-var loadIframeOnDemand = loadIframeOnDemand || function(containerId, iframeSrc, iframeClasses) {
-    console.log("Executing loadIframeOnDemand", containerId, iframeSrc, iframeClasses)
-    const container = document.getElementById(containerId);
+window.document.onload = function(e){
+    console.log('[GlanceLib/LIOD] "Load iFrame on demand" loaded.')
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
+            const container = entry.target;
             if (entry.isIntersecting) {
                 if (!container.querySelector('iframe')) {
                     const iframe = document.createElement('iframe');
-                    iframe.src = iframeSrc;
-                    iframe.class = iframeClasses;
+                    iframe.src = container.getAttribute('data-lib-liod-src');
+                    iframe.className = container.getAttribute('data-lib-liod-classes')
+                    iframe.classList.add('glance-lib-liod');
                     container.appendChild(iframe);
-                    console.log("iframe loaded");
+                    console.log('iframe loaded');
                 }
             } else {
                 const iframe = container.querySelector('iframe');
                 if (iframe) {
                     iframe.remove();
-                    console.log("iframe unloaded");
+                    console.log('iframe unloaded');
                 }
             }
         });
     }, {
+        root: null,
         threshold: 0.1
     })
-    observer.observe(container);
+    document.querySelector('[data-lib-liod]').forEach(elem => {
+        console.log('[GlanceLib/LIOD] Observing: ', elem);
+        elem.classList.add('glance-lib-liod')
+        observer.observe(elem);
+    });
 }
