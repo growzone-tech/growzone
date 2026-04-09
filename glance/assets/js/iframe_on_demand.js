5f300fb3b672c4ed78cb20069be10ccd32f082f8
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-04-09 19:20:15 +0200

    tries window.onload

--- glance/assets/js/iframe_on_demand.js
+++ glance/assets/js/iframe_on_demand.js
@@ -1,6 +1,7 @@
 console.log('[GlanceLib/LIOD] Library: "Load iFrame on demand".');
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
     console.log('[GlanceLib/LIOD] Initializing...');
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
@@ -8,8 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
             if (entry.isIntersecting) {
                 if (!container.querySelector('iframe')) {
                     const iframe = document.createElement('iframe');
-                    iframe.src = container.getAttribute('data-lib-liod-src');
-                    iframe.className = container.getAttribute('data-lib-liod-classes')
+                    iframe.src = container.dataset.libLiodSrc;
+                    iframe.className = container.dataset.libLiodClasses;
                     iframe.classList.add('glance-lib-liod');
                     container.appendChild(iframe);
                     console.log('iframe loaded');
@@ -26,10 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
         root: null,
         threshold: 0.1
     });
-    document.querySelectorAll('[data-lib-liod]').forEach(elem => {
+    document.querySelectorAll('div[data-lib-liod]').forEach(elem => {
         console.log('[GlanceLib/LIOD] Observing: ', elem);
         elem.classList.add('glance-lib-liod')
         observer.observe(elem);
     });
     console.log('[GlanceLib/LIOD] Initialized.');
-});
+}//);
