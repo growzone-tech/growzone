0368d3877060353e2cb9193f15bc4e7d7d048afc
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-04-09 19:06:52 +0200

    test if window.document.onload is executed

--- glance/assets/js/iframe_on_demand.js
+++ glance/assets/js/iframe_on_demand.js
@@ -1,5 +1,7 @@
-window.document.onload = function(e){
-    console.log('[GlanceLib/LIOD] "Load iFrame on demand" loaded.')
+console.log('[GlanceLib/LIOD] Library: "Load iFrame on demand".');
+
+window.document.onload = function(e) {
+    console.log('[GlanceLib/LIOD] Initializing...');
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             const container = entry.target;
@@ -23,10 +25,11 @@ window.document.onload = function(e){
     }, {
         root: null,
         threshold: 0.1
-    })
+    });
     document.querySelector('[data-lib-liod]').forEach(elem => {
         console.log('[GlanceLib/LIOD] Observing: ', elem);
         elem.classList.add('glance-lib-liod')
         observer.observe(elem);
     });
+    console.log('[GlanceLib/LIOD] Initialized.');
 }
